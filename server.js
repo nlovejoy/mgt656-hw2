09b687c14cb3d4f854cafa647ab996f03ebd549a
nflovejoy <nflovejoy@gmail.com>
2015-10-09 03:02:16 +0000

    add nickname route -Lovejoy

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a total ninja!!!');
 });
+
+app.get('/nickname', function(request, response){
+    response.send('pleasant-herring');
+});
+
 
 app.listen(process.env.PORT || 4000);
